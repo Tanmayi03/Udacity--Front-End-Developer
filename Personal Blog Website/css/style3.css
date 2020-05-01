5f8445b161a9327e8fe92b14db596a4027d7f893
Tanmayi <45629438+Tanmayi03@users.noreply.github.com>
2020-05-01 11:12:51 +0530

    Create style3.css

---
+++ Personal Blog Website/css/style3.css
@@ -0,0 +1,20 @@
+.bio {
+    justify-content: center;
+    padding: auto;
+    margin-left: 20px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    width: 100%;
+}
+
+h2 {
+    color: crimson;
+}
+
+.para {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
+}
+
+h3 {
+    color: red;
+}
